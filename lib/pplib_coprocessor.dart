48abb1b936c02fd739ca9eeb1408031dff412ac4
Michael Jansen <mjansen4857@gmail.com>
2023-11-15 16:40:04 -0500

    dynamic obstacles

--- lib/pplib_coprocessor.dart
+++ lib/pplib_coprocessor.dart
@@ -11,23 +11,23 @@ void runPathfinding() async {
   NT4Client ntClient =
       NT4Client(serverBaseAddress: '127.0.0.1'); // TODO: allow config
 
-  DateTime startTime = DateTime.now();
-
   NT4Subscription navGridJsonSub = ntClient.subscribe(
-      '/PPLibCoprocessor/navGrid',
+      '/PPLibCoprocessor/RemoteADStar/navGrid',
       NT4SubscriptionOptions(periodicRateSeconds: 0.001, all: true));
-  NT4Subscription startPosSub = ntClient.subscribe('/PPLibCoprocessor/startPos',
+  NT4Subscription startPosSub = ntClient.subscribe(
+      '/PPLibCoprocessor/RemoteADStar/startPos',
       NT4SubscriptionOptions(periodicRateSeconds: 0.001, all: true));
-  NT4Subscription goalPosSub = ntClient.subscribe('/PPLibCoprocessor/goalPos',
+  NT4Subscription goalPosSub = ntClient.subscribe(
+      '/PPLibCoprocessor/RemoteADStar/goalPos',
       NT4SubscriptionOptions(periodicRateSeconds: 0.001, all: true));
-  // TODO: dynamic obstacles
+  NT4Subscription dynamicObsSub = ntClient.subscribe(
+      '/PPLibCoprocessor/RemoteADStar/dynamicObstacles',
+      NT4SubscriptionOptions(periodicRateSeconds: 0.001, all: true));
 
   NT4Topic pathTopic = ntClient.publishNewTopic(
-      '/PPLibCoprocessor/pathPoints', NT4TypeStr.typeFloat64Arr);
+      '/PPLibCoprocessor/RemoteADStar/pathPoints', NT4TypeStr.typeFloat64Arr);
 
   Pathfinder pathfinder = ADStar(pathGeneratedCallback: (points) {
-    Duration generationTime = DateTime.now().difference(startTime);
-    print('Generation took ${generationTime.inMicroseconds / 1000.0}ms');
     try {
       List<double> ntPoints = [];
       for (Point p in points) {
@@ -37,7 +37,6 @@ void runPathfinding() async {
 
       ntClient.addSample(pathTopic, ntPoints);
     } catch (_) {}
-    print('path generated');
   });
 
   StreamSubscription navGridStream = navGridJsonSub.stream().listen((sample) {
@@ -59,8 +58,25 @@ void runPathfinding() async {
     if (sample != null && sample is List) {
       try {
         Point goalPos = Point(sample[0] as num, sample[1] as num);
-        startTime = DateTime.now();
         pathfinder.setGoalPosition(goalPos);
+      } catch (_) {}
+    }
+  });
+  StreamSubscription dynamicObsStream = dynamicObsSub.stream().listen((sample) {
+    if (sample != null && sample is List) {
+      try {
+        // First two numbers are the robot's current position
+        Point currentPos = Point(sample[0] as num, sample[1] as num);
+
+        // Remaining points describe bounding boxes
+        List<(Point, Point)> obs = [];
+        for (int i = 2; i <= sample.length - 4; i += 4) {
+          Point p1 = Point(sample[i] as num, sample[i + 1] as num);
+          Point p2 = Point(sample[i + 2] as num, sample[i + 3] as num);
+          obs.add((p1, p2));
+        }
+
+        pathfinder.setDynamicObstacles(obs, currentPos);
       } catch (_) {}
     }
   });
@@ -68,6 +84,7 @@ void runPathfinding() async {
   await Future.wait([
     navGridStream.asFuture(),
     startPosStream.asFuture(),
-    goalPosStream.asFuture()
+    goalPosStream.asFuture(),
+    dynamicObsStream.asFuture(),
   ], eagerError: true);
 }
